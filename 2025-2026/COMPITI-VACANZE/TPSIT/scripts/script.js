ee3253c65c2d8e673b3c1ac5b8ac9b9aa8498145
Perassi Lorenzo <perassi.lorenzo1804@gmail.com>
2026-08-27 16:23:05 +0200

    Add initial implementation of "Battaglia Navale" game
    - Created index.html for the game interface with a grid and control buttons.
    - Developed script.js to handle game logic, including grid generation, cell clicking, and tracking attempts and time.
    - Added style.css for basic styling of the game elements, including grid layout and button design.

---
+++ 2025-2026/COMPITI-VACANZE/TPSIT/scripts/script.js
@@ -0,0 +1,86 @@
+let naviIniziali = [
+    { row: 0, col: 0 },
+    { row: 1, col: 0 },
+    { row: 2, col: 0 }
+];
+
+let navi = [...naviIniziali];
+
+let count = 0;
+let divCount = document.getElementById("tries");
+
+let remain = navi.length;
+let divRemain = document.getElementById("remain");
+
+let time = 0;
+let divTime = document.getElementById("time");
+
+let timerId;
+
+function Grid() {
+
+    navi = [...naviIniziali];
+    document.getElementById("grid").innerHTML = "";
+
+    for (let col = 0; col < 8; col++) {
+        for (let row = 0; row < 8; row++) {
+            document.getElementById("grid").innerHTML += `<div class="cell" onclick="Cell(${row}, ${col}, this)"></div>`;
+        }
+    }
+
+    document.getElementById("btn-start").style.display = "none";
+
+    count = 0;
+    remain = navi.length;
+    time = 0;
+
+    divCount.textContent = `Tentativi: ${count}`;
+    divRemain.textContent = `Caselle rimaste: ${remain}`;
+    divTime.textContent = `Tempo: ${time}`;
+
+    clearInterval(timerId);
+
+    timerId = setInterval(() => {
+        time++;
+        divTime.textContent = `Tempo: ${time}`;
+    }, 1000);
+}
+
+function Cell(row, col, cell) {
+
+    if (cell.classList.contains("clicked")) {
+        return;
+    }
+
+    cell.classList.add("clicked");
+    count++;
+
+    let found = false;
+    let foundIndex = -1;
+
+    for (let i = 0; i < navi.length; i++) {
+        if (navi[i].row === row && navi[i].col === col) {
+            found = true;
+            foundIndex = i;
+
+            break;
+        }
+    }
+
+    if (found) {
+        cell.style.backgroundColor = "red";
+        remain--;
+        navi.splice(foundIndex, 1);
+    } else {
+        cell.style.backgroundColor = "#00e1ff";
+    }
+
+    divCount.textContent = `Tentativi: ${count}`;
+    divRemain.textContent = `Caselle rimaste: ${remain}`;
+
+    if (remain === 0) {
+
+        document.getElementById("final-message").textContent = `Hai trovato tutte le navi in ${count} tentativi e ${time} secondi!`;
+        document.getElementById("btn-start").style.display = "block";
+    }
+}

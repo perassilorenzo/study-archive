24d4fda60581776fa79180c962ddab879e8cfe39
Perassi Lorenzo <perassi.lorenzo1804@gmail.com>
2026-09-17 16:38:00 +0200

    Update project files for 0917: modify dependency file, enhance main program with file reading/writing, and add layout configuration

--- 2026-2027/SISTEMI&RETI/0917/main.c
+++ 2026-2027/SISTEMI&RETI/0917/main.c
@@ -21,12 +21,41 @@ void main()
     fp = fopen("file.txt", "w");
 
     printf("\nInserisci il nome ==> ");
+
     //scanf("%s", name);
     gets(name);
-
     fprintf(fp, "%s", name);
-
-
     fclose(fp);
+
+    // read - 1
+    fp = fopen("file.txt", "r");
+    fscanf(fp, "%s", name);
+    fclose(fp);
+    printf("\nNome: \"%s\"", name);
+
+    // read - 2
+    fp = fopen("file.txt", "r");
+    while(fgets(name, 30, fp) != NULL
+          printf("\nNome: \"%s\"", name);
+    fclose(fp);
+
+    // read - 3
+    fp = fopen("file.txt", "r");
+    while(!feof(fp)){
+        fscanf(fp, "%s", name);
+        printf("\nNome: \"%s\"", name);
+    }
+    fclose(fp);
+
+    // controllo file elenco
+    fp = fopen("list.txt", "r");
+    if (fp == NULL)
+    {
+        fclose(fp);
+        fp = fopen("list.txt", "w");
+    }
+    fclose(fp);
+
+
     printf("\n\n");
 }

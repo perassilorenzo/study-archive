f0beba3db82c8c88459f31a859986bbd3c603375
Perassi Lorenzo <perassi.lorenzo1804@gmail.com>
2026-09-23 11:53:12 +0200

    0923 | esercizio 4 [da concludere]

---
+++ 2026-2027/INFORMATICA/2026/0923-esercizio4/0923-esercizio4/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace _0923_esercizio4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.inserisciStudenteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gestioneStudentiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ricercaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eliminaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gestioneClasseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.visualizzaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ordinaAZToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salvaSuFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.caricaDaFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.esciToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.inserisciStudenteToolStripMenuItem,
+            this.gestioneStudentiToolStripMenuItem,
+            this.gestioneClasseToolStripMenuItem,
+            this.esciToolStripMenuItem,
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // inserisciStudenteToolStripMenuItem
+            // 
+            this.inserisciStudenteToolStripMenuItem.Name = "inserisciStudenteToolStripMenuItem";
+            this.inserisciStudenteToolStripMenuItem.Size = new System.Drawing.Size(138, 24);
+            this.inserisciStudenteToolStripMenuItem.Text = "Inserisci Studente";
+            // 
+            // gestioneStudentiToolStripMenuItem
+            // 
+            this.gestioneStudentiToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ricercaToolStripMenuItem,
+            this.eliminaToolStripMenuItem});
+            this.gestioneStudentiToolStripMenuItem.Name = "gestioneStudentiToolStripMenuItem";
+            this.gestioneStudentiToolStripMenuItem.Size = new System.Drawing.Size(140, 24);
+            this.gestioneStudentiToolStripMenuItem.Text = "Gestione Studenti";
+            // 
+            // ricercaToolStripMenuItem
+            // 
+            this.ricercaToolStripMenuItem.Name = "ricercaToolStripMenuItem";
+            this.ricercaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.ricercaToolStripMenuItem.Text = "Ricerca";
+            // 
+            // eliminaToolStripMenuItem
+            // 
+            this.eliminaToolStripMenuItem.Name = "eliminaToolStripMenuItem";
+            this.eliminaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.eliminaToolStripMenuItem.Text = "Elimina";
+            // 
+            // gestioneClasseToolStripMenuItem
+            // 
+            this.gestioneClasseToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.visualizzaToolStripMenuItem,
+            this.ordinaAZToolStripMenuItem,
+            this.salvaSuFileToolStripMenuItem,
+            this.caricaDaFileToolStripMenuItem});
+            this.gestioneClasseToolStripMenuItem.Name = "gestioneClasseToolStripMenuItem";
+            this.gestioneClasseToolStripMenuItem.Size = new System.Drawing.Size(126, 24);
+            this.gestioneClasseToolStripMenuItem.Text = "Gestione Classe";
+            // 
+            // visualizzaToolStripMenuItem
+            // 
+            this.visualizzaToolStripMenuItem.Name = "visualizzaToolStripMenuItem";
+            this.visualizzaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.visualizzaToolStripMenuItem.Text = "Visualizza ";
+            // 
+            // ordinaAZToolStripMenuItem
+            // 
+            this.ordinaAZToolStripMenuItem.Name = "ordinaAZToolStripMenuItem";
+            this.ordinaAZToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.ordinaAZToolStripMenuItem.Text = "Ordina (A-Z)";
+            // 
+            // salvaSuFileToolStripMenuItem
+            // 
+            this.salvaSuFileToolStripMenuItem.Name = "salvaSuFileToolStripMenuItem";
+            this.salvaSuFileToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.salvaSuFileToolStripMenuItem.Text = "Salva su File";
+            // 
+            // caricaDaFileToolStripMenuItem
+            // 
+            this.caricaDaFileToolStripMenuItem.Name = "caricaDaFileToolStripMenuItem";
+            this.caricaDaFileToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.caricaDaFileToolStripMenuItem.Text = "Carica da File";
+            // 
+            // esciToolStripMenuItem
+            // 
+            this.esciToolStripMenuItem.Name = "esciToolStripMenuItem";
+            this.esciToolStripMenuItem.Size = new System.Drawing.Size(48, 24);
+            this.esciToolStripMenuItem.Text = "Esci";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(30, 24);
+            this.toolStripMenuItem1.Text = "?";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "0923 | esercizio 4 (Gestione STUDENTI / CLASSI)";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem inserisciStudenteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gestioneStudentiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ricercaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eliminaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gestioneClasseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem visualizzaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ordinaAZToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salvaSuFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem caricaDaFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem esciToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ 2026-2027/INFORMATICA/2026/0923-esercizio4/0923-esercizio4/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _0923_esercizio4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Struct Studente
+        /// </summary>
+        public struct Studente
+        {
+            // definisco le caratteristiche
+            public int NumeroMatricola;
+            public string Cognome;
+            public string Nome;
+            public DateTime DataNascita;
+            public string Classe;
+            public string Specializzazione;
+
+            // costruttore
+            public Studente(int nMatricola, string cognome, string nome, DateTime dataNascita, string classe, string specializzazione)
+            {
+                NumeroMatricola = nMatricola;
+                Cognome = cognome;
+                Nome = nome;
+                DataNascita = dataNascita;
+                Classe = classe;
+                Specializzazione = specializzazione;
+            }
+
+            // pulizia dati studente
+            public void Clear()
+            {
+                NumeroMatricola = 0;
+                Cognome = string.Empty;
+                Nome = string.Empty;
+                DataNascita = DateTime.MinValue;
+                Classe = string.Empty;
+                Specializzazione= string.Empty;
+            }
+
+            // calcolo eta studente
+            public int CalcoloEta()
+            {
+                int eta;
+
+                eta = DateTime.Now.Year - DataNascita.Year;
+
+                return eta;
+            }
+
+            // dati studente 
+            public string GetStudente()
+            {
+                return $"N° Matricola: {NumeroMatricola}\n" +
+                    $"Cognome: {Cognome}\n" +
+                    $"Nome: {Nome}\n" +
+                    $"Data di nascita: {DataNascita.ToString(@"dd\/MM\/yyyy")}\n" +
+                    $"Classe: {Classe}\n" +
+                    $"Specializzazione: {Specializzazione}";
+            }
+
+            // dati studente scrittura su file
+            public override string ToString()
+            {
+                return NumeroMatricola.ToString() + ";";
+
+                // DA CONCLUDERE
+            }
+
+        }
+
+
+
+    }
+}
